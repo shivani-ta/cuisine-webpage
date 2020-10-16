9dff6d74254b7a7b97ecfb41ef36aa8b82507f40
shivani <tangellapallyshivani@gmail.com>
2020-10-16 14:54:20 -0500

    First version of application moved into github

---
+++ express-locallibrary-tutorial-master/models/author.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");  //for date handling
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
+});
+
+// Virtual for author "full" name.
+AuthorSchema.virtual('name').get(function() {
+  var fullname = '';
+
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+  }
+
+  if (!this.first_name && !this.family_name) {
+    fullname = '';
+  }
+  return fullname;
+});
+
+// Virtual for this author instance URL.
+AuthorSchema.virtual('url').get(function() {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifetime_string = '';
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  }
+  lifetime_string += ' - ';
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); //format 'YYYY-MM-DD'
+});
+
+// Export model.
+module.exports = mongoose.model('Author', AuthorSchema);
+
